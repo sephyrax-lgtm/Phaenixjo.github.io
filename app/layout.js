7a442b56fb56f05fbf96960700bd069124c7a9d4
Akila <akilabanajoseph@gmail.com>
2025-12-06 18:55:07 +0100

    Fix hydration + update Next.js + clean build

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -19,9 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.className} ${geistMono.className} antialiased`}>
         {children}
       </body>
     </html>
